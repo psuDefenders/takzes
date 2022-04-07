acd06cc4359cf1a7d28d408a59559f941e8f0b1d
LuhePxl <unbelivableplayer@outlook.com>
2022-04-07 16:49:27 -0400

    fuck
    LETS FUCING GO

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -8,15 +8,37 @@ var combo = 0
 var shop_delay = 0
 var shop = false
 
+var mail = false
+var gotten_mail
+var kind_of_mail
+var mail_spam
+var mail_count = 0
+
 func _on_Timer_timeout():
 	score += getmoney
+
+func _on_MinTimer_timeout():
+	gotten_mail = randi() % 19 #1/20 chance every minute
+	if gotten_mail == 19 && mail_count < 11:
+		kind_of_mail = randi() % 3 #0 = stock info, #1 = ads on shop(sales), #2 = news, #3 = spam
+		if kind_of_mail == 0:
+			print()
+			mail_count += 1
+		elif kind_of_mail == 1:
+			print()
+			mail_count += 1
+		elif kind_of_mail == 2:
+			print()
+			mail_count += 1
+		elif kind_of_mail == 3:
+			mail_spam = randi() % 50
+			mail_count += 1
 
 func _process(_delta):
 	$Score.text = str(score)
 	
 	if shop_delay > 0:
 		shop_delay -= 1
-
 
 func _on_Click_pressed():
 	$ClickTimer.start()
@@ -28,7 +50,6 @@ func _on_Click_pressed():
 	if combo <= 25:
 		score += money
 
-
 func _on_ClickTimer_timeout():
 	combo = 0
 	$ComboEffect.emitting = false # Effects
@@ -39,9 +60,16 @@ func _on_Shop_pressed():
 		shop_delay = 20
 		$AnimationShop.play("shop")
 		$AnimationShop.play("TheShop")
-	if shop == true and shop_delay ==0:
+	elif shop == true and shop_delay ==0:
 		shop = false
 		shop_delay = 20
 		$AnimationClose.play("shop")
 		$AnimationClose.play("TheShop")
-	
+
+func _on_Mail_pressed():
+	if mail == false:
+		mail = true
+		$TheMail.visible = true 
+	elif mail == true:
+		mail = false
+		$TheMail.visible = false
--- scripts/shop.gd
+++ scripts/shop.gd
@@ -4,10 +4,10 @@ onready var score# = get_tree().get_root().get_node("Control").score
 onready var money# = get_tree().get_root().get_node("Control").money
 onready var getmoney# = get_tree().get_root().get_node("Control").getmoney
 
-onready var MPS = get_node("../scene/Game/MPS")
-onready var MPC = get_node("../scene/Game/MPC")
-var MPCRequirement = 20
-var MPSRequirement = 20
+onready var MPS = get_tree().get_root().get_node("Control/MPS")
+onready var MPC = get_tree().get_root().get_node("Control/MPC")
+var MPC1Needed = 20
+var MPS1Needed = 20
 
 func _process(_delta):
 	score = get_tree().get_root().get_node("Control").score
@@ -15,18 +15,18 @@ func _process(_delta):
 	getmoney = get_tree().get_root().get_node("Control").getmoney
 
 func _on_MPC1_pressed():
-	if score >= MPCRequirement:
-		score -= MPCRequirement
-		MPCRequirement = round(MPCRequirement * 1.4)
-		#money = money + 1
-		$ShopList/MPC1.text = str("+1 MPC [", MPCRequirement, "]") #Combine multiple strings to show the required clicks.
-		MPC.text = str("MPC:", money)
+	if score >= MPC1Needed:
+		get_tree().get_root().get_node("Control").score -= MPC1Needed
+		MPC1Needed = round(MPC1Needed * 1.4)
+		get_tree().get_root().get_node("Control").money = get_tree().get_root().get_node("Control").money + 1
+		$ShopList/MPC1.text = str("+1 MPC [", MPC1Needed, "]")
+		MPC.text = str("MPC: ", money +1)
 
 func _on_MPS1_pressed():
-	if score >= MPSRequirement:
-		score -= MPSRequirement
-		MPSRequirement = round(MPSRequirement * 1.4)
-		getmoney = getmoney + 1
-		$ShopList/MPS1.text = str("+1 MPS [", MPSRequirement, "]") #Combine multiple strings to show the required clicks.
-		MPS.text = str("MPS:", to_string(getmoney)getmoney)
+	if score >= MPS1Needed:
+		get_tree().get_root().get_node("Control").score -= MPS1Needed
+		MPS1Needed = round(MPS1Needed * 1.4)
+		get_tree().get_root().get_node("Control").getmoney = get_tree().get_root().get_node("Control").getmoney + 1
+		$ShopList/MPS1.text = str("+1 MPS [", MPS1Needed, "]")
+		MPS.text = str("MPS: ", getmoney +1)
 		

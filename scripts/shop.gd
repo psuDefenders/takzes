7eb16dbe97f4147a42adf77ea92846433da94a95
LuhePxl <unbelivableplayer@outlook.com>
2022-04-24 20:55:06 -0400

    :moyai: :moneybag: sell mode
    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -15,7 +15,7 @@ var StickNeeded = 10
 var PlantNeeded = 30
 var CdplayerNeeded = 150
 
-var SELLMODE = false
+var SELLINGMODE = false
 
 func _process(_delta):
 	score = get_tree().get_root().get_node("Control").score
@@ -37,10 +37,12 @@ func _process(_delta):
 #		$ScrollContainer/ShopList/MoneyPrinter/Name.set("custom_colors/font_color",Color(255,0,0))
 
 func _on_sellmode_pressed():
-	if SELLMODE == false:
-		SELLMODE = true
-	elif SELLMODE == true:
-		SELLMODE = false
+	if SELLINGMODE == false:
+		SELLINGMODE = true
+		$ScrollContainer/ShopList/info/sell/ModeOn.text = "Sell Mode : On"
+	elif SELLINGMODE == true:
+		SELLINGMODE = false
+		$ScrollContainer/ShopList/info/sell/ModeOn.text = "Sell Mode : Off"
 
 func _on_MPC1_pressed():
 	if score >= MPC1Needed:
@@ -59,7 +61,7 @@ func _on_MPS1_pressed():
 		MPS.text = str("MPS: ", getmoney +1)
 
 func _on_BuyMoneyPrinter_pressed():
-	if checking >= MoneyPrinterNeeded:
+	if SELLINGMODE == false and checking >= MoneyPrinterNeeded:
 		get_tree().get_root().get_node("Control").checking -= MoneyPrinterNeeded
 		MoneyPrinterNeeded = round(MoneyPrinterNeeded * 1.4)
 		get_tree().get_root().get_node("Control").getmoney = get_tree().get_root().get_node("Control").getmoney + 50
@@ -68,9 +70,18 @@ func _on_BuyMoneyPrinter_pressed():
 		$ScrollContainer/ShopList/MoneyPrinter/amount.text = str("x", get_tree().get_root().get_node("Control").moneyPrinter)
 		MPS.text = str("MPS: ", getmoney +50)
 		Global.livingExpense += 30
+	elif SELLINGMODE == true  and get_tree().get_root().get_node("Control").moneyPrinter != 0:
+		get_tree().get_root().get_node("Control").checking += round(MoneyPrinterNeeded*0.1)
+		MoneyPrinterNeeded = round(MoneyPrinterNeeded / 1.4)
+		get_tree().get_root().get_node("Control").getmoney = get_tree().get_root().get_node("Control").getmoney - 50
+		$ScrollContainer/ShopList/MoneyPrinter/Price.text = str(MoneyPrinterNeeded , "$")
+		get_tree().get_root().get_node("Control").moneyPrinter -= 1
+		$ScrollContainer/ShopList/MoneyPrinter/amount.text = str("x", get_tree().get_root().get_node("Control").moneyPrinter)
+		MPS.text = str("MPS: ", getmoney -50)
+		Global.livingExpense -= 30
 
 func _on_BuyStick_pressed():
-	if checking >= StickNeeded:
+	if SELLINGMODE == false and checking >= StickNeeded:
 		get_tree().get_root().get_node("Control").checking -= StickNeeded
 		StickNeeded = round(StickNeeded * 1.4)
 		get_tree().get_root().get_node("Control").money = get_tree().get_root().get_node("Control").money + 1
@@ -79,11 +90,19 @@ func _on_BuyStick_pressed():
 		$ScrollContainer/ShopList/Stick/amount.text = str("x", get_tree().get_root().get_node("Control").stick)
 		MPC.text = str("MPC: ", money +1)
 		Global.livingExpense += 0
+	elif SELLINGMODE == true  and get_tree().get_root().get_node("Control").stick != 0:
+		get_tree().get_root().get_node("Control").checking += round(StickNeeded*0.1)
+		StickNeeded = round(StickNeeded / 1.4)
+		get_tree().get_root().get_node("Control").money = get_tree().get_root().get_node("Control").money - 1
+		$ScrollContainer/ShopList/Stick/Price.text = str(StickNeeded , "$")
+		get_tree().get_root().get_node("Control").stick -= 1
+		$ScrollContainer/ShopList/Stick/amount.text = str("x", get_tree().get_root().get_node("Control").stick)
+		MPC.text = str("MPC: ", money -1)
+		Global.livingExpense -= 0
 
 
 func _on_BuyPlant_pressed():
-	print("plant")
-	if checking >= PlantNeeded:
+	if SELLINGMODE == false and checking >= PlantNeeded:
 		get_tree().get_root().get_node("Control").checking -= PlantNeeded
 		PlantNeeded = round(PlantNeeded * 1.4)
 		get_tree().get_root().get_node("Control").getmoney = get_tree().get_root().get_node("Control").getmoney + 2
@@ -92,10 +111,18 @@ func _on_BuyPlant_pressed():
 		$ScrollContainer/ShopList/Plant/amount.text = str("x", get_tree().get_root().get_node("Control").plant)
 		MPS.text = str("MPS: ", getmoney +2)
 		Global.livingExpense += 0
-
+	elif SELLINGMODE == true and get_tree().get_root().get_node("Control").plant != 0:
+		get_tree().get_root().get_node("Control").checking += round(PlantNeeded*0.1)
+		PlantNeeded = round(PlantNeeded / 1.4)
+		get_tree().get_root().get_node("Control").getmoney = get_tree().get_root().get_node("Control").getmoney - 2
+		$ScrollContainer/ShopList/Plant/Price.text = str(PlantNeeded , "$")
+		get_tree().get_root().get_node("Control").plant -= 1
+		$ScrollContainer/ShopList/Plant/amount.text = str("x", get_tree().get_root().get_node("Control").plant)
+		MPS.text = str("MPS: ", getmoney -2)
+		Global.livingExpense -= 0
 
 func _on_BuyCD_pressed():
-	if checking >= CdplayerNeeded:
+	if SELLINGMODE == false and checking >= CdplayerNeeded:
 		get_tree().get_root().get_node("Control").checking -= CdplayerNeeded
 		CdplayerNeeded = round(CdplayerNeeded * 1.4)
 		get_tree().get_root().get_node("Control").money = get_tree().get_root().get_node("Control").money + 4
@@ -104,4 +131,12 @@ func _on_BuyCD_pressed():
 		$ScrollContainer/ShopList/CD_Player/amount.text = str("x", get_tree().get_root().get_node("Control").cdPlayer)
 		MPC.text = str("MPC: ", money +4)
 		Global.livingExpense += 20
-
+	elif SELLINGMODE == true and get_tree().get_root().get_node("Control").cdPlayer != 0:
+		get_tree().get_root().get_node("Control").checking += round(CdplayerNeeded*0.1)
+		CdplayerNeeded = round(CdplayerNeeded / 1.4)
+		get_tree().get_root().get_node("Control").money = get_tree().get_root().get_node("Control").money - 4
+		$ScrollContainer/ShopList/CD_Player/Price.text = str(CdplayerNeeded , "$")
+		get_tree().get_root().get_node("Control").cdPlayer -= 1
+		$ScrollContainer/ShopList/CD_Player/amount.text = str("x", get_tree().get_root().get_node("Control").cdPlayer)
+		MPC.text = str("MPC: ", money -4)
+		Global.livingExpense -= 20

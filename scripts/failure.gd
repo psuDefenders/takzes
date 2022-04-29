baa3d6fe1a1e9483ca2272c22d32de417e4f6e7f
PurplFish <jimlego64@gmail.com>
2022-04-29 16:19:39 -0400

    GAME OVER

--- scripts/failure.gd
+++ scripts/failure.gd
@@ -7,8 +7,12 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func failure():
+	visible = true
+	Global.lore = 0
+	Global.firstTime = true
+	Global.livingExpense = 40
+	Global.extraliving = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

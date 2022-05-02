b489457238318b8b7bc20433cc5732bdb454caa3
LuhePxl <unbelivableplayer@outlook.com>
2022-05-01 20:26:32 -0400

    sdas
    sadsa

--- scripts/housing.gd
+++ scripts/housing.gd
@@ -5,7 +5,6 @@ var dictNeededHousing = {"dumbster":500 , "cardboard_box":2500 , "tent":8800, "v
 
 
 func _on_Goback_pressed():
-	get_tree().get_root().get_node("Control/housing").visible = false
 	get_tree().get_root().get_node("Control").closehousing()
 
 

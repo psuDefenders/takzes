e022a9e2b7de070be7ff61c42ac1caa732cec6b6
PurplFish <jimlego64@gmail.com>
2022-05-01 21:18:55 -0400

    shop is fixed thanks jeesus

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -14,13 +14,25 @@ var MPS1Needed = 20
 var MoneyPrinterNeeded = 50
 
 onready var dictElements = {"Stick":$ScrollContainer/ShopList/Stick/amount , "Plant":30 , "CD":150, "64":0, "Wifi":0, "Gilberts":0,"Phone":0,"dumbells":0,"Keyboard":0,"fastFood":0,"edison":0,"car":0,"kimberly":0,"employee":0,"robot":0,"boombox":0,"airflower":0,"trapmusic":0,"classicalusic":0,"airFryer":0, "moneyLaunderer":0,"moneyPrinter":0}
-var dictNeeded = {"Stick":10 , "Plant":30 , "CD":150, "64":240, "Wifi":0, "Gilberts":0,"Phone":0,"dumbells":0,"Keyboard":250,"fastFood":0,"edison":500,"car":0,"kimberly":0,"employee":0,"robot":0,"boombox":0,"airflower":0,"trapmusic":0,"classicalusic":0,"airFryer":0, "moneyLaunderer":0,"moneyPrinter":0}
+var dictNeeded = {"Stick":10 , "Plant":30 , "CD":150, "64":240, "Wifi":500, "Gilberts":0,"Phone":1000,"dumbells":70,"Keyboard":250,"fastFood":13.50,"edison":500,"car":0,"kimberly":0,"employee":0,"robot":0,"boombox":0,"airflower":0,"trapmusic":0,"classicalusic":0,"airFryer":0, "moneyLaunderer":0,"moneyPrinter":0}
 var StickNeeded = 10
 var PlantNeeded = 30
 var CdplayerNeeded = 150
 
 var Capacity = 0
 var SELLINGMODE = false
+
+func _physics_process(delta):
+	check_availlable("Stick")
+	check_availlable("Plant")
+	check_availlable("CD")
+	check_availlable("64")
+	check_availlable("Wifi")
+	check_availlable("Phone")
+	check_availlable("Keyboard")
+	check_availlable("fastFood")
+	check_availlable("edison")
+
 
 #func _process(_delta):
 #	score = get_tree().get_root().get_node("Control").score
@@ -48,6 +60,14 @@ func _on_sellmode_pressed():
 	elif SELLINGMODE == true:
 		SELLINGMODE = false
 		$ScrollContainer/ShopList/info/sell/ModeOn.text = "Sell Mode : Off"
+
+func check_availlable(item_needed):
+	if SELLINGMODE == false and game.checking >= dictNeeded[item_needed] and game.Capacity < game.maxCapacity:
+		get_node("ScrollContainer/ShopList/"+ item_needed+"/Name").modulate= Color(1,1,1)
+	elif SELLINGMODE == true  and game.dictAmount[item_needed] != 0:
+		get_node("ScrollContainer/ShopList/"+ item_needed+"/Name").modulate= Color(1,1,1)
+	else:
+		get_node("ScrollContainer/ShopList/"+ item_needed+"/Name").modulate= Color(1,0,0)
 
 func buy_sell(item_needed, mpc, mps, liveCost):
 	if SELLINGMODE == false and game.checking >= dictNeeded[item_needed] and game.Capacity < game.maxCapacity:
